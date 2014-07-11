6eb5eaae85d3799f1c81740d336569919975ea19
Doa <doajafri@gmail.com>
2014-07-11 18:35:09 -0400

    added error modal

--- js/app.js
+++ js/app.js
@@ -26,16 +26,17 @@ var app = (function(document, $) {
 
 // redirects based on passcode entered
 $(function() {
-	$('.gbu').click(function () {
+	'use strict'
+	$('.button').click(function () {
 		// grab input, make all letters lowercase
 		var input = ($('input:text').val()).toLowerCase();
 		// evaluate input, append to URL if valid
-		if (input == "hello") {
-			window.location.href = "http://www.hellodoa.com/" + input;
+		if (input === 'hello') {
+			window.location.href = 'http://www.hellodoa.com/' + input;
 		}
 		// give error if not valid
 		else {
-			alert("Incorrect Access Code");
+			$('#wrongCode').foundation('reveal', 'open');
 		}
 	});
 });
