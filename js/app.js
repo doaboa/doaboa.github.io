09d9453dab15322732f2beeb7c95780a5b96ce9a
Doa <doajafri@gmail.com>
2014-07-15 17:14:35 -0400

    added specific pages

--- js/app.js
+++ js/app.js
@@ -24,9 +24,13 @@ var app = (function(document, $) {
 
 })();
 
-// redirects based on passcode entered
+
+
 $(function() {
 	'use strict'
+
+
+// ***** MAIN PAGE ***** redirects based on passcode entry
 
 	// allows the enter key to also trigger the button click event
 	$(".enterAccess").keypress(function(event){
@@ -43,6 +47,7 @@ $(function() {
 		var input = ($('input:text').val()).toLowerCase();
 		// evaluate input to see if it is contained in codes, if so append to URL if valid
 		if ( codes.indexOf(input) !== -1 ) {
+			// can use document.URL instead of manual URL once I get godaddy to route correctly
 			window.location.href = document.URL + input + '.html';
 		}
 		// if not contained, open error modal
@@ -50,4 +55,11 @@ $(function() {
 			$('#wrongCode').foundation('reveal', 'open');
 		}
 	});
+
+
+// ***** RESUME PAGE *****
+
+
+
+
 });
