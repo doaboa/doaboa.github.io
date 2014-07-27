a8a3d9e19710256956983ae447dc5fcd76bccf7b
Doa <doajafri@gmail.com>
2014-07-27 18:33:19 -0400

    navigation works, header completed, footer created, sections and heading created

--- js/app.js
+++ js/app.js
@@ -60,9 +60,21 @@ $(function() {
 		$('.sneakPeek').css('opacity', '1');
 	});
 
+	$('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash,
+        $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top -100
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+
 
 // ***** RESUME PAGE *****
-
 
 
 
